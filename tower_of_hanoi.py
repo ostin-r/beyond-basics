807b94721dd51c8de844c3554b6b9bc99cc5f03a
Austin Richards <austinjrichards@outlook.com>
2021-07-13 05:25:24 -0600

    ran black to reformat code spacing

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -1,23 +1,24 @@
-'''
+"""
 A practice project by Al Sweigart- copied from Chapter 14 of 
 Beyond the Basics with Python.  This is an exercise in understanding
 the structure of typical OOP programs.
 
 TOWER OF HANOI, a stack-moving puzzle game
-'''
+"""
 
 import copy
 import sys
 
-TOTAL_DISKS = 5 # More disks means a more difficult puzzle
+TOTAL_DISKS = 5  # More disks means a more difficult puzzle
 
 # Start with all the disks on tower A
 SOLVED_TOWER = list(range(TOTAL_DISKS, 0, -1))
 
+
 def main():
-    '''Runs a single game of Tower of Hanoi.'''
+    """Runs a single game of Tower of Hanoi."""
     print(
-        '''THE TOWER OF HANOI, by Al Sweigart al@inventwithpython.com
+        """THE TOWER OF HANOI, by Al Sweigart al@inventwithpython.com
 A stack-moving puzzle game.
 
 Move the tower of disks, one disk at a time, to another tower. Larger
@@ -25,10 +26,10 @@ disks cannot rest on top of a smaller disk.  The goal is to recreate
 the stack on tower A on tower B or C.
 
 More info at: https://en.wikipedia.org/wiki/Tower_of_Hanoi
-'''
+"""
     )
 
-    '''
+    """
     The towers dictionary has keys "A", "B", and "C" and values
     that are lists representing a tower of disks. The list contains
     integers representing disks of different sizes, and the start of
@@ -38,11 +39,11 @@ More info at: https://en.wikipedia.org/wiki/Tower_of_Hanoi
     larger disk on top of a smaller disk and is an invalid
     configuration. The list [3, 1] is allowed since smaller disks
     can go on top of larger ones.
-    '''
+    """
 
-    towers = {'A': copy.copy(SOLVED_TOWER), 'B': [], 'C': []}
+    towers = {"A": copy.copy(SOLVED_TOWER), "B": [], "C": []}
 
-    while True: # Run a single turn on each iteration of this loop.
+    while True:  # Run a single turn on each iteration of this loop.
         # Display the towers and disks:
         display_towers(towers)
 
@@ -54,28 +55,28 @@ More info at: https://en.wikipedia.org/wiki/Tower_of_Hanoi
         towers[to_tower].append(disk)
 
         # Check if the player has solved the puzzle:
-        if SOLVED_TOWER in (towers['B'], towers['C']):
-            display_towers(towers) # Display the towers one last time.
-            print('You have solved the puzzle! Well done!')
+        if SOLVED_TOWER in (towers["B"], towers["C"]):
+            display_towers(towers)  # Display the towers one last time.
+            print("You have solved the puzzle! Well done!")
             sys.exit()
 
 
 def get_player_move(towers: list) -> list:
-    '''Asks the player for a move. Returns (from_tower, to_tower).'''
+    """Asks the player for a move. Returns (from_tower, to_tower)."""
 
-    while True: # Keep asking the player until they enter a valid move.
+    while True:  # Keep asking the player until they enter a valid move.
         print('Enter the letters of "from" tower and "To" tower, or QUIT')
-        print('(e.g., AB moves a disk from tower A to tower B.')
+        print("(e.g., AB moves a disk from tower A to tower B.")
         print()
-        response = input('> ').upper().strip()
+        response = input("> ").upper().strip()
 
         if response == "QUIT":
-            print('Thanks for playing!')
+            print("Thanks for playing!")
             sys.exit()
 
         # Make sure the response is valid.
-        if response not in ('AB', 'AC', 'BA', 'BC', 'CA', 'CB'):
-            print('Enter one of the following: AB, AC, BA, BC, CA, CB')
+        if response not in ("AB", "AC", "BA", "BC", "CA", "CB"):
+            print("Enter one of the following: AB, AC, BA, BC, CA, CB")
             continue
 
         # Switch to more descriptive response names.
@@ -83,7 +84,7 @@ def get_player_move(towers: list) -> list:
 
         if len(towers[from_tower]) == 0:
             # The "from" tower cannot be empty.
-            print('You selected a tower with no disks.')
+            print("You selected a tower with no disks.")
             continue
         elif len(towers[to_tower]) == 0:
             return from_tower, to_tower
@@ -96,35 +97,35 @@ def get_player_move(towers: list) -> list:
 
 
 def display_towers(towers: list) -> None:
-    '''Display the three towers and their disks.'''
+    """Display the three towers and their disks."""
 
     for level in range(TOTAL_DISKS, -1, -1):
-        for tower in (towers['A'], towers['B'], towers['C']):
+        for tower in (towers["A"], towers["B"], towers["C"]):
             if level >= len(tower):
-                display_disk(0) # Display the bare pole.
+                display_disk(0)  # Display the bare pole.
             else:
-                display_disk(tower[level]) # Display the disk.
+                display_disk(tower[level])  # Display the disk.
         print()
 
     # Display the tower labels: A, B, and C.
-    empty_space = ' ' * TOTAL_DISKS
-    print('{0} A{0}{0} B{0}{0} C\n'.format(empty_space))
+    empty_space = " " * TOTAL_DISKS
+    print("{0} A{0}{0} B{0}{0} C\n".format(empty_space))
 
 
 def display_disk(width: int) -> None:
-    '''Display the disk of a given width. A width of 0 means no disk.'''
-    empty_space = ' ' * (TOTAL_DISKS - width)
+    """Display the disk of a given width. A width of 0 means no disk."""
+    empty_space = " " * (TOTAL_DISKS - width)
 
     if width == 0:
         # display the section of the pole without a disk
-        print(f'{empty_space}||{empty_space}', end='')
+        print(f"{empty_space}||{empty_space}", end="")
 
     else:
         # display the disk with a number label
         disk = "@" * width
-        number_label = str(width).rjust(2, '_')
-        print(f'{empty_space}{disk}{number_label}{disk}{empty_space}', end='')
+        number_label = str(width).rjust(2, "_")
+        print(f"{empty_space}{disk}{number_label}{disk}{empty_space}", end="")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
